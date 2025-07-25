55103b2f325c8000f804c47c2a6d7150e82da12f
PNTCP <nattaponchanprom@gmail.com>
2025-07-25 15:20:17 +0700

    last

--- ex4.java
+++ ex4.java
@@ -5,6 +5,26 @@ public class ex4{
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please Enter Num1: ");
-        double num1 = scanner.nextDouble();//
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Please Enter Num2: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Please Enter Num3: ");
+        double num3 = scanner.nextDouble();
+
+        System.out.print("Please Enter Num4: ");
+        double num4 = scanner.nextDouble();
+
+        System.out.print("Please Enter Num5: ");
+        double num5 = scanner.nextDouble();
+
+        double sum = num1 + num2 + num3 + num4 + num5;
+        double average = sum/5;
+
+        System.out.println("Summary = " + sum);
+        System.out.println("Average = " + average);
+
+        scanner.close();
     }
 }
