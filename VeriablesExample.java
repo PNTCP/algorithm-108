01642a12d6042d8b30272e6205d0bb00086f1c40
PNTCP <nattaponchanprom@gmail.com>
2025-07-25 16:03:27 +0700

    VeriablesExample

---
+++ VeriablesExample.java
@@ -0,0 +1,20 @@
+public class VeriablesExample {
+    public static void main(String[] args){
+        char clothingSize = 's';
+        int websiteVisitors = 0;
+        char studentGrade = 'A';
+        double studentGPA = 4.0;
+        final double PI = 3.14159;
+        float annualExpenses = 15000.00f;
+        final int MONTHS_IN_YEAR = 12;
+
+        //Example
+        System.out.println("Clothing Size: " + clothingSize);
+        System.out.println("Website Visitors: " + websiteVisitors);
+        System.out.println("Student Grade:" + studentGrade);
+        System.out.println("Student GPA: " + studentGPA);
+        System.out.println("Value of Pi: " + PI);
+        System.out.println("Annual Expenses: " + annualExpenses);
+        System.out.println("Mount in a Year: " + MONTHS_IN_YEAR);
+    }
+}
